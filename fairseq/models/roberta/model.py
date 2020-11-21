3614977bdc0edf655428da87a32100f5b0074203
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-21 21:49:00 +0800

    Update model.py

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -279,6 +279,7 @@ class RobertaClassificationHead(nn.Module):
 
     def __init__(self, input_dim, inner_dim, num_classes, activation_fn, pooler_dropout):
         super().__init__()
+        pdb.set_trace()
         self.dense = nn.Linear(input_dim, inner_dim)
         self.activation_fn = utils.get_activation_fn(activation_fn)
         self.dropout = nn.Dropout(p=pooler_dropout)
