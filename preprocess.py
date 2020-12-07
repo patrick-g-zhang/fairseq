7ad9b599a6d1b5d2ba399f59f2f027ce9e88d871
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-12-07 22:04:27 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -119,22 +119,22 @@ def main(args):
             pool = Pool(processes=num_workers - 1)
             for worker_id in range(1, num_workers):
                 prefix = "{}{}".format(output_prefix, worker_id)
-                pdb.set_trace()
-                binarize(args, input_file, vocab, prefix, lang,
-                         offsets[worker_id], offsets[worker_id + 1])
-                # pool.apply_async(
-                #     binarize,
-                #     (
-                #         args,
-                #         input_file,
-                #         vocab,
-                #         prefix,
-                #         lang,
-                #         offsets[worker_id],
-                #         offsets[worker_id + 1]
-                #     ),
-                #     callback=merge_result
-                # )
+                # pdb.set_trace()
+                # binarize(args, input_file, vocab, prefix, lang,
+                         # offsets[worker_id], offsets[worker_id + 1])
+                pool.apply_async(
+                    binarize,
+                    (
+                        args,
+                        input_file,
+                        vocab,
+                        prefix,
+                        lang,
+                        offsets[worker_id],
+                        offsets[worker_id + 1]
+                    ),
+                    callback=merge_result
+                )
             pool.close()
 
         ds = indexed_dataset.make_builder(dataset_dest_file(args, output_prefix, lang, "bin"),
