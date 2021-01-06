2d116144a1f2b38c0dd82ccf9a4c835d0d21e7cb
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-01-06 20:21:18 +0800

    Update __init__.py

--- fairseq/data/__init__.py
+++ fairseq/data/__init__.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .dictionary import Dictionary, TruncatedDictionary
+from .dictionary import Dictionary, TruncatedDictionary, PhonemeDictionary
 
 from .fairseq_dataset import FairseqDataset, FairseqIterableDataset
 
