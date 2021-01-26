d1d7409ec08e7e9f0d3510857c450e2ce29bf4ef
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-01-27 01:36:12 +0800

    Update iterators.py

--- fairseq/data/iterators.py
+++ fairseq/data/iterators.py
@@ -168,6 +168,8 @@ class EpochBatchIterator(EpochBatchIterating):
     ):
         assert isinstance(dataset, torch.utils.data.Dataset)
         self.dataset = dataset
+        print("-------------dsdsds-----------------------")
+        print(dataset)
         self.collate_fn = collate_fn
         self.frozen_batches = tuple(batch_sampler)
         self.seed = seed
