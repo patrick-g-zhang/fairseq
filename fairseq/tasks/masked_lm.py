5ab227faa050f48d74ed0e27490902907d9516b6
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-01-26 22:36:31 +0800

    Update masked_lm.py

--- fairseq/tasks/masked_lm.py
+++ fairseq/tasks/masked_lm.py
@@ -114,7 +114,7 @@ class MaskedLMTask(FairseqTask):
         assert len(paths) > 0
         data_path = paths[epoch % len(paths)]
         split_path = os.path.join(data_path, split)
-        print("********************************************")
+        print("line masked_lm 117 ********************************************")
         print(split_path)
         if self.args.two_inputs:
             dataset = data_utils.load_two_indexed_datasets(
