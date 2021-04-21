63631b10c55ecb628ea2aabfaee26c519c3893ca
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-04-21 22:32:27 +0800

    Update masked_lm.py

--- fairseq/criterions/masked_lm.py
+++ fairseq/criterions/masked_lm.py
@@ -156,8 +156,8 @@ class MaskedLmLoss(FairseqCriterion):
                 # dur loss
                 dur_gt = sample['target']['dur_gt']
                 loss_dur = dur_loss(
-                    dur_pred, dur_gt, sample['net_input']['src_tokens']['phoneme']) * self.args.prosody_loss_coeff
-                loss += loss_dur.detach()
+                    dur_pred.detach(), dur_gt, sample['net_input']['src_tokens']['phoneme']) * self.args.prosody_loss_coeff
+                loss += loss_dur
                 logging_output['loss_dur'] = utils.item(
                     loss_dur.data) if reduce else loss_dur.data
 
