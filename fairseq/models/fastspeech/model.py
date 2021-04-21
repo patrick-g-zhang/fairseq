fba7e677942e817d9503c30255c69f75791011f6
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-04-21 22:40:00 +0800

    Update model.py

--- fairseq/models/fastspeech/model.py
+++ fairseq/models/fastspeech/model.py
@@ -77,9 +77,8 @@ class DurationPredictor(torch.nn.Module):
                 xs = xs * (1 - x_masks.type(xs.dtype))[:, None, :]
 
         # NOTE: calculate in log domain
-        xs = self.linear(xs.transpose(1, -1)).squeeze(-1)  # (B, Tmax)
+        xs = self.linear(xs.transpose(1, -1)).squeeze(-1).to(xs.device)  # (B, Tmax)
 
-        xs = xs.detach()
         if x_masks is not None:
             xs = xs.masked_fill(x_masks, 0.0).to(xs.device)
         return xs
