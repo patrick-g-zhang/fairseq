21cf074bca243c5e96f81bf07a90062e7bc3cdc5
ZHANG GUANGYAN <guangyanee@gmail.com>
2021-05-02 04:16:16 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -123,30 +123,30 @@ def main(args):
             pool = Pool(processes=num_workers - 1)
             for worker_id in range(1, num_workers):
                 prefix = "{}{}".format(output_prefix, worker_id)
-                # pool.apply_async(
-                #     binarize,
-                #     (
-                #         args,
-                #         input_file,
-                #         vocab,
-                #         prefix,
-                #         lang,
-                #         offsets[worker_id],
-                #         offsets[worker_id + 1],
-                #         vocabb,
-                #     ),
-                #     callback=merge_result
-                # )
+                pool.apply_async(
+                    binarize,
+                    (
+                        args,
+                        input_file,
+                        vocab,
+                        prefix,
+                        lang,
+                        offsets[worker_id],
+                        offsets[worker_id + 1],
+                        vocabb,
+                    ),
+                    callback=merge_result
+                )
                 # pdb.set_trace()
-                binarize(args,
-                         input_file,
-                         vocab,
-                         prefix,
-                         lang,
-                         offsets[worker_id],
-                         offsets[worker_id + 1],
-                         vocabb,
-                         ),
+                # binarize(args,
+                #          input_file,
+                #          vocab,
+                #          prefix,
+                #          lang,
+                #          offsets[worker_id],
+                #          offsets[worker_id + 1],
+                #          vocabb,
+                #          ),
             pool.close()
 
         ds = indexed_dataset.make_builder(dataset_dest_file(args, output_prefix, lang, "bin"),
@@ -284,7 +284,6 @@ def main(args):
 def binarize(args, filename, vocab, output_prefix, lang, offset, end, vocabb=None, append_eos=False):
 
     # ds is MMapIndexedDatasetBuilder for indexed Dataset builder
-    pdb.set_trace()
     ds = indexed_dataset.make_builder(dataset_dest_file(args, output_prefix, lang, "bin"),
                                       impl=args.dataset_impl, vocab_size=len(vocab))
 
